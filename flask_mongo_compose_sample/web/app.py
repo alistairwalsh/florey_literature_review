a678dc59785129f0e6eb3c612ee04d64f54df7f7
alistairwalsh <alistair.walsh@me.com>
2016-11-28 16:27:18 +1100

    removed mongo config info

--- flask_mongo_compose_sample/web/app.py
+++ flask_mongo_compose_sample/web/app.py
@@ -4,9 +4,6 @@ from flask_pymongo import PyMongo
 
 app = Flask(__name__)
 
-app.config['MONGO_DBNAME'] = 'restdb'
-app.config['MONGO_URI'] = 'mongodb://0.0.0.0:27017/restdb'
-
 mongo = PyMongo(app)
 
 @app.route('/javascript')
