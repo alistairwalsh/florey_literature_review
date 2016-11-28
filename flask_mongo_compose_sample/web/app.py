ee068f01281060d55749a15a9d0c83103bf28d4c
alistairwalsh <alistair.walsh@me.com>
2016-11-28 17:11:15 +1100

    trying for config values

--- flask_mongo_compose_sample/web/app.py
+++ flask_mongo_compose_sample/web/app.py
@@ -11,7 +11,7 @@ mongo = PyMongo(app)
 def info():
 	username = "Alistair"
 	#user = mongo.db.users.find_one_or_404({'_id': username})
-	return username
+	return mongo.config['MONGO_ARCHIVE_COLLECTION']
 
 @app.route('/javascript')
 def javascript():
