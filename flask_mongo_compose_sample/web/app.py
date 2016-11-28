e1e722d521ab99e6492cd4b980b90b4ff4cd98d2
alistairwalsh <alistair.walsh@me.com>
2016-11-28 16:25:06 +1100

    Changed local host to 0.0.0.0

--- flask_mongo_compose_sample/web/app.py
+++ flask_mongo_compose_sample/web/app.py
@@ -5,7 +5,7 @@ from flask_pymongo import PyMongo
 app = Flask(__name__)
 
 app.config['MONGO_DBNAME'] = 'restdb'
-app.config['MONGO_URI'] = 'mongodb://localhost:27017/restdb'
+app.config['MONGO_URI'] = 'mongodb://0.0.0.0:27017/restdb'
 
 mongo = PyMongo(app)
 
