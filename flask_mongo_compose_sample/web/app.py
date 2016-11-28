4b1d86062a5e532371bd8922c8785561f4fe43ef
alistairwalsh <alistair.walsh@me.com>
2016-11-28 16:38:26 +1100

    trying basic info post

--- flask_mongo_compose_sample/web/app.py
+++ flask_mongo_compose_sample/web/app.py
@@ -6,6 +6,10 @@ app = Flask(__name__)
 
 mongo = PyMongo(app)
 
+@app.route('/info')
+def info():
+	print('hello')
+
 @app.route('/javascript')
 def javascript():
     return render_template('javascript_tutorial.html')
@@ -18,15 +22,15 @@ def index():
 def signin():
 	return render_template('showSignUp.html')
 
-@app.route('/add_article', methods=['POST'])
-def add_article():
-  article = mongo.db.articles
-  name = request.json['name']
-  author = request.json['author']
-  article_id = article.insert({'name': name, 'author': author})
-  new_article = article.find_one({'_id': author_id })
-  output = {'name' : new_article['name'], 'author' : new_article['author']}
-  return jsonify({'result' : output})
+# @app.route('/add_article', methods=['POST'])
+# def add_article():
+#   article = mongo.db.articles
+#   name = request.json['name']
+#   author = request.json['author']
+#   article_id = article.insert({'name': name, 'author': author})
+#   new_article = article.find_one({'_id': author_id })
+#   output = {'name' : new_article['name'], 'author' : new_article['author']}
+#   return jsonify({'result' : output})
 
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0')
