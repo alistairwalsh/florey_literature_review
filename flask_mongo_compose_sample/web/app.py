b6adc2bb3288a722ad5dea4a7e7310b99118fb8f
alistairwalsh <alistair.walsh@me.com>
2016-11-28 16:16:25 +1100

    added mongo config settings

--- flask_mongo_compose_sample/web/app.py
+++ flask_mongo_compose_sample/web/app.py
@@ -1,7 +1,12 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, jsonify, request
 from flask_pymongo import PyMongo
 
+
 app = Flask(__name__)
+
+app.config['MONGO_DBNAME'] = 'restdb'
+app.config['MONGO_URI'] = 'mongodb://localhost:27017/restdb'
+
 mongo = PyMongo(app)
 
 @app.route('/javascript')
